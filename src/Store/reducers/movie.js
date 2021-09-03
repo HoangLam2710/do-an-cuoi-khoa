a1936284eaf74361a3abb81bc008df2d64032cd7
Quoc Huy <pqhuy0211@gmail.com>
2021-09-03 16:45:02 +0700

    Movie Detail Layout

--- src/Store/reducers/movie.js
+++ src/Store/reducers/movie.js
@@ -1,29 +1,33 @@
 import { actionTypes } from "../actions/types";
 
 const initialState = {
-    movieList: [],
-    movieDetail: null,
-    page: 1,
-    banner: [],
+  movieList: [],
+  movieDetail: null,
+  page: 1,
+  banner: [],
+  showtimes: [],
 };
 
 const reducer = (state = initialState, { type, payload }) => {
-    switch (type) {
-        case actionTypes.SET_MOVIES:
-            state.movieList = payload;
-            return { ...state };
-        case actionTypes.SET_DETAIL_MOVIE:
-            state.movieDetail = payload;
-            return { ...state };
-        case actionTypes.SET_PAGE:
-            state.page = payload;
-            return { ...state };
-        case actionTypes.SET_BANNER:
-            state.banner = payload;
-            return { ...state };
-        default:
-            return state;
-    }
+  switch (type) {
+    case actionTypes.SET_MOVIES:
+      state.movieList = payload;
+      return { ...state };
+    case actionTypes.SET_DETAIL_MOVIE:
+      state.movieDetail = payload;
+      return { ...state };
+    case actionTypes.SET_PAGE:
+      state.page = payload;
+      return { ...state };
+    case actionTypes.SET_BANNER:
+      state.banner = payload;
+      return { ...state };
+    case actionTypes.SET_SHOWTIMES:
+      state.showtimes = payload;
+      return { ...state };
+    default:
+      return state;
+  }
 };
 
 export default reducer;
